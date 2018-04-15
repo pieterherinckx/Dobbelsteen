7bcba6871f03475a5ff8fe4b41475303a121dbd6
pieterherinckx <pieterherinckx@atheneumtienen.be>
2018-04-15 16:59:13 +0200

    Initial commit

---
+++ Dobbelsteen.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+/**
+ * Write a description of class Dobbelsteen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dobbelsteen
+{
+    private int max;
+    private int resultaat;
+    private Random rg;
+    
+    public Dobbelsteen(int max){
+        this.max = max;
+        this.resultaat = 0;
+        this.rg = new Random();
+    }
+    
+    public int getResultaat(){
+        return resultaat;
+    }
+    
+    public void gooi(){
+        resultaat = rg.nextInt(max)+1;    
+    }
+}
